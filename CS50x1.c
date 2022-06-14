1ad71567eecdb9c3eeba6e34025db294f80ba69c
Vivian De Mitry <107444675+viviandemitry@users.noreply.github.com>
2022-06-13 21:57:19 -0300

    Update CS50x1.c

--- CS50x1.c
+++ CS50x1.c
@@ -1,6 +1,9 @@
+#include <cs50.h>
 #include <stdio.h>
 
-int main (void)
+int ain (void)
 {
-    printf("Hello\n")
+    int x = get_int("x: ");
+    int y = get_int("y: ");
+    printf("%i\n", x + y);
 }
